6fccf25224ea4032ae792be5a8b5274d8aabae77
reluc <relu.cri@gmail.com>
2019-11-13 17:49:56 +0100

    fix: default secure client initialization

---
+++ test/secure.js
@@ -0,0 +1,9 @@
+const secure = require("../lib/secure")
+const assert = require("assert")
+describe('Secure initizalition unit tests', () => {
+    it('should merge configurations', () => {
+        const client =  secure.defaultClient("a","b",{host :"another"})
+        assert.equal(client.config.host,"another")
+    });
+    
+});
