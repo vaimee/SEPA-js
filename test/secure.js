a5bc45b6cdf0809bb16e56dffd2a10d8b86e3e84
reluc <relu.cri@gmail.com>
2019-11-21 15:45:41 +0100

    fix: remove old test

--- test/secure.js
+++ test/secure.js
@@ -1,13 +1,10 @@
 const secure = require("../lib/secure")
 const assert = require("assert")
-const axios = require('axios')
+
 describe('Secure initizalition unit tests', () => {
     it('should merge configurations', () => {
         const client =  secure.defaultClient("a","b",{host :"another"})
         assert.equal(client.config.host,"another")
-    });
-    it('should merge configurations', async () => {
-		await axios.get("https://sepa.vaimee.it:8443/echo")
     });
     
 });
