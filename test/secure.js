0ef226d70ff0e8b863cbaa1aa3a22bd160a5c639
reluc <relu.cri@gmail.com>
2019-12-04 12:46:44 +0100

    fix: error forwarding in secure client

--- test/secure.js
+++ test/secure.js
@@ -6,5 +6,17 @@ describe('Secure initizalition unit tests', () => {
         const client =  secure.defaultClient("a","b",{host :"another"})
         assert.equal(client.config.host,"another")
     });
+
+    it('should forward execption', (done) => {
+        const client =  secure.defaultClient("a","b",{host :"another"})
+        client._autoRenewToken(()=>{
+            return Promise.reject(new Error("Not ok"))
+        }).then(() =>{
+            console.log("hallo");
+            done(new Error("Error not forwarded"))
+        }).catch(()=>{
+            done()
+        })
+    });
     
 });
