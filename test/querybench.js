01af2e58d8ea7273d1e898d49d6e4b4190e06e0d
reluc <relu.cri@gmail.com>
2019-12-09 10:56:29 +0100

    fix: binding replament
    Now varible names can be followed by commas or
    semicolums

--- test/querybench.js
+++ test/querybench.js
@@ -123,6 +123,24 @@ describe('querybench', function() {
     })
     assert.equal("select * where{?a ?ab 10.}", query)
   });
+  it('it should replace variable even if it is followed by ; or , (colum or comma)' , function () {
+    let bench = new Bench({})
+    let comma = bench.sparql("select * where{?a ?ab ?c,}", {
+      c : {
+        type: "literal",
+        value: 10
+      }
+    })
+    let colum = bench.sparql("select * where{?a ?ab ?c;}", {
+      c: {
+        type: "literal",
+        value: 10
+      }
+    })
+
+    assert.equal("select * where{?a ?ab 10,}", comma)
+    assert.equal("select * where{?a ?ab 10;}", colum)
+  });
   it('it should just replace variables' , function () {
     bench = new Bench({})
     query = bench.sparql("select * where{?a ?ab ?c. ab:la <la> <lab>}", {
